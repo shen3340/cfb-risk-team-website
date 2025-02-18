2f312c78b5f7151a334b5196365b7c8da6ebfb3c
shen3340 <shen3340@gmail.com>
2025-02-18 13:46:29 -0500

    ok this works locally, just run backend and frontend terminal

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
 const Form = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   const handleLoginClick = () => {
-    // Redirect to your backend's /auth/discord route
-    window.location.href = "http://localhost:3001/auth/discord";
+    if (!isAuthenticated) {
+      // Redirect to your backend's /auth/discord route if not authenticated
+      window.location.href = "http://localhost:3001/auth/discord";
+      setIsAuthenticated(true);
+    } else {
+      // Optionally, handle what happens if the user is already authenticated
+      console.log("User is already authenticated");
+    }
   };
 
   return (
